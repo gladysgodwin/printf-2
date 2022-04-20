eda257ab86c674d48b2f815f84aa12408c0d88ed
SanusiWasiu <was.sanusi@gmail.com>
2022-04-21 00:14:52 +0100

    add header file

--- main.h
+++ main.h
@@ -6,6 +6,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct specifier - struct of format specifier
+ * @spec: format character
+ * @func: function for conversion
+ */
+typedef struct specifier {
+    char *spec;
+    int (*func)(va_list);
+} formatSpec;
 
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
 
 #endif
