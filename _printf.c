7e13a849bb275423ba2328659177e4ad6e86ebe7
SanusiWasiu <was.sanusi@gmail.com>
2022-04-21 01:03:37 +0100

    initialize printf recreation file

--- _printf.c
+++ _printf.c
@@ -1,4 +1,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * _printf - recreation of C stdlib function, printf
+ * @format: string with format specifier
+ * Return: number of characters printed
+ */
+
+ int _printf(const char *format, ...)
+ {
+     
+ }
 
